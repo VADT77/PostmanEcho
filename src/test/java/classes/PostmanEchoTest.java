239efc9f87b729f7c4c7d10feed5094d5f6a8c17
VADT77 <turanosoff@gmail.com>
2023-02-27 00:30:19 +0300

    "worktest"

--- src/test/java/classes/PostmanEchoTest.java
+++ src/test/java/classes/PostmanEchoTest.java
@@ -16,6 +16,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("1ПостманЭхотест "));
+                .body("data", equalTo("ПостманЭхотест "));
     }
 }
